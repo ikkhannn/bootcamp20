59c3d4d44963f83ff2e7886f292bf8affcf04e0d
Imran Khan <imrankhannn81@gmail.com>
2020-06-07 21:51:48 +0500

    update yaml

---
+++ react-base-app/build/precache-manifest.53747f3eca2eebdf75a0e2a464fbad41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d075fcc1ad586e3f88239cf02ea1af58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d9f270131ca080370cf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da8792238028bd1be30b",
+    "url": "/static/js/2.251b5031.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.251b5031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9f270131ca080370cf",
+    "url": "/static/js/main.6b0ce468.chunk.js"
+  },
+  {
+    "revision": "f5c81d5837c304670226",
+    "url": "/static/js/runtime-main.67db746e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
